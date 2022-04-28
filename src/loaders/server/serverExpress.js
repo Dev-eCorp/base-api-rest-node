1174c86d1b4352582b973fa614271b84cb036074
Eduard Cruz <salikintres2001@gmail.com>
2022-04-28 17:16:05 -0500

    Se agregan los logger

--- src/loaders/server/serverExpress.js
+++ src/loaders/server/serverExpress.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const logger = require('../logger');
 const config = require('../../config');
 
 class ServerExpress{
@@ -10,12 +11,14 @@ class ServerExpress{
 
         this._middlewares();
         this._routes();
-    }
+        this._notFound();
+        this._errorHandler();
+    };
 
     _middlewares() {
         this.app.use(express.json());
         this.app.use(morgan('tiny'));
-    }
+    };
 
     _routes() {
         this.app.head('/status', (req, res) => {
@@ -23,17 +26,40 @@ class ServerExpress{
         });
 
         this.app.use(this.basePathUsers, require('../../routes/users'));
-    }
+    };
+
+    _notFound() {
+        this.app.use((req, res, next) => {
+            const err = new Error('Not Found');
+            err.code = 404;
+            next(err);
+        });
+    };
+
+    _errorHandler() {
+        this.app.use((err, req, res, next) => {
+            const code = err.code || 500;
+            res.status(code);
+
+            const body = {
+                error: {
+                    code,
+                    message: err.message
+                }
+            }
+            res.status(code).json(body);
+        });
+    };
 
     async start() {
         this.app.listen(this.port, (error) => {
             if (error) {
-                console.log(error);
+                logger.error(error);
                 process.exit(1);
                 return;
             }
         });
-    }
-}
+    };
+};
 
 module.exports = ServerExpress;
